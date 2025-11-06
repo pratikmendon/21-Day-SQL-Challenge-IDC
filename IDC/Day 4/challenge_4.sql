7c660ad7e19fe8fa1436e065077fb48a67ddcb44
PRATIK MENDON <pratikmendon@gmail.com>
2025-11-06 09:52:55 +0530

    Add files via upload
    Added Day 4 challenge

---
+++ IDC/Day 4/challenge_4.sql
@@ -0,0 +1,9 @@
+/*
+Find the 3rd to 7th highest patient satisfaction scores from the patients table, 
+showing patient_id, name, service, and satisfaction. Display only these 5 records.
+*/
+
+SELECT patient_id, `name`,service, satisfaction 
+FROM patients
+ORDER BY satisfaction DESC
+LIMIT 5 OFFSET 2;
